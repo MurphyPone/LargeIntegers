52bb16a12c645dde128a3fca859ca09d7c1f86aa
MurphyPone <murphyP1@bishopireton.org>
2017-09-14 16:21:37 -0400

    As far as I can get before scratching my head no longer increases my progress

--- LargeIntegers/src/Handler.java
+++ LargeIntegers/src/Handler.java
@@ -1,4 +1,31 @@
-
 public class Handler {
-
+	private int size;
+	
+	//Constructors
+	public Handler() {size = 30;}
+	public Handler(int length) { size = length; }
+	
+	public int[] add(int[] a, int[] b) {
+		int[] result = new int[size];
+		
+		//THIS WILL NOT CARRY OVER, NEED MORE COMPLEX
+		for(int i = 0; i < size; i ++) {
+			result[i] = a[i] + b[i];
+		}
+		return result;
+	}
+	
+	//Iterate backwards and carry numbers >9
+	public int[] addComplex(int[] a, int[] b) {
+		int[] result = new int[size];
+	
+		for(int i = size; i > 0; i --) {
+			if (a[i] + b[i] < 10)
+				result[i] = a[i] + b[i];
+			else //need to carry
+				result[i-1] ++;
+				result[i] = result[i]%10;
+		}
+		return result;
+	}
 }
--- LargeIntegers/src/LargeIntegerTester.java
+++ LargeIntegers/src/LargeIntegerTester.java
@@ -3,7 +3,27 @@ public class LargeIntegerTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		//Creates two tester arrays of integers
+		int[] x = new int[] {1,2,3,4,5};
+		int[] y = new int[] {9,9,9,9,9};
+		
+		//Creates new Handler 
+		Handler addEmUp = new Handler(x.length);
+		
+		for(int i = 0; i < x.length; i ++) {
+			System.out.println("Simple Addition Position: " + i + " = " + addEmUp.add(x,  y)[i]);
+		}
+		
+		//COMPLEX ADDITION 
+		for(int i = 0; i < x.length; i ++) {
+			System.out.println("Complex Addition Position: " + i + " = " + addEmUp.addComplex(x,  y)[i]);
+		}
+		
+		//DISHES
+		//CHARLIE 5:30-7 
+		//CHARLIE 7 @ ST.Louis
+
 	}
 
 }
